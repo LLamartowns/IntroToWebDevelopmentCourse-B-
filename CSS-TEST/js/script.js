532f392e14aa8803b9275b3c4a797201ad3d295e
LaMar Towns <LLamartowns@gmail.com>
2016-07-06 16:34:59 -0400

    Add files via upload

---
+++ CSS-TEST/js/script.js
@@ -0,0 +1,279 @@
+window.ansSubmit1 = function() {
+	var ans = document.getElementById("a3").checked;
+  var checkboxs = document.getElementsByClassName("question1");
+  var okay = false;
+  for (var i = 0; i < checkboxs.length; i++) {
+    if (checkboxs[i].checked) {
+      okay = true;
+      
+    } else {
+      
+    }
+
+  }
+  // Check if none of the boxes were checked
+  if (!okay) {
+    document.getElementById("ansSubmit1").style.backgroundColor="red";
+   	window.alert("Please select an answer");
+  }else if(ans !== okay){
+  document.getElementById("ansSubmit1").style.backgroundColor="red";
+  window.alert("Try Again")
+  }else { document.getElementById("ansSubmit1").style.backgroundColor="green";
+  }
+}
+
+window.ansSubmit2 = function() {
+	var ans = document.getElementById("a8").checked;
+  var checkboxs = document.getElementsByClassName("question2");
+  var okay = false;
+  for (var i = 0; i < checkboxs.length; i++) {
+    if (checkboxs[i].checked) {
+      okay = true;
+      
+    } else {
+      
+    }
+
+  }
+  // Check if none of the boxes were checked
+  if (!okay) {
+    document.getElementById("ansSubmit2").style.backgroundColor="red";
+   	window.alert("Please select an answer");
+  }else if(ans !== okay){
+  document.getElementById("ansSubmit2").style.backgroundColor="red";
+  window.alert("Try Again")
+  }else { document.getElementById("ansSubmit2").style.backgroundColor="green";
+  }
+}
+
+window.ansSubmit3 = function() {
+	var ans = document.getElementById("a10").checked;
+  var checkboxs = document.getElementsByClassName("question3");
+  var okay = false;
+  for (var i = 0; i < checkboxs.length; i++) {
+    if (checkboxs[i].checked) {
+      okay = true;
+      
+    } else {
+      
+    }
+
+  }
+  // Check if none of the boxes were checked
+  if (!okay) {
+    document.getElementById("ansSubmit3").style.backgroundColor="red";
+   	window.alert("Please select an answer");
+  }else if(ans !== okay){
+  document.getElementById("ansSubmit3").style.backgroundColor="red";
+  window.alert("Try Again")
+  }else { document.getElementById("ansSubmit3").style.backgroundColor="green";
+  }
+}
+
+window.ansSubmit4 = function() {
+	var ans = document.getElementById("a13").checked;
+  var checkboxs = document.getElementsByClassName("question4");
+  var okay = false;
+  for (var i = 0; i < checkboxs.length; i++) {
+    if (checkboxs[i].checked) {
+      okay = true;
+      
+    } else {
+      
+    }
+
+  }
+  // Check if none of the boxes were checked
+  if (!okay) {
+    document.getElementById("ansSubmit4").style.backgroundColor="red";
+   	window.alert("Please select an answer");
+  }else if(ans !== okay){
+  document.getElementById("ansSubmit4").style.backgroundColor="red";
+  window.alert("Try Again")
+  }else { document.getElementById("ansSubmit4").style.backgroundColor="green";
+  }
+}
+
+window.ansSubmit5 = function() {
+	var ans = document.getElementById("a17").checked;
+  var checkboxs = document.getElementsByClassName("question5");
+  var okay = false;
+  for (var i = 0; i < checkboxs.length; i++) {
+    if (checkboxs[i].checked) {
+      okay = true;
+      
+    } else {
+      
+    }
+
+  }
+  // Check if none of the boxes were checked
+  if (!okay) {
+    document.getElementById("ansSubmit5").style.backgroundColor="red";
+   	window.alert("Please select an answer");
+  }else if(ans !== okay){
+  document.getElementById("ansSubmit5").style.backgroundColor="red";
+  window.alert("Try Again")
+  }else { document.getElementById("ansSubmit5").style.backgroundColor="green";
+  }
+}
+
+window.ansSubmit6 = function() {
+	var ans = document.getElementById("a21").checked;
+  var checkboxs = document.getElementsByClassName("question6");
+  var okay = false;
+  for (var i = 0; i < checkboxs.length; i++) {
+    if (checkboxs[i].checked) {
+      okay = true;
+      
+    } else {
+      
+    }
+
+  }
+  // Check if none of the boxes were checked
+  if (!okay) {
+    document.getElementById("ansSubmit6").style.backgroundColor="red";
+   	window.alert("Please select an answer");
+  }else if(ans !== okay){
+  document.getElementById("ansSubmit6").style.backgroundColor="red";
+  window.alert("Try Again")
+  }else { document.getElementById("ansSubmit6").style.backgroundColor="green";
+  }
+}
+
+window.ansSubmit7 = function() {
+	var ans = document.getElementById("a26").checked;
+  var checkboxs = document.getElementsByClassName("question7");
+  var okay = false;
+  for (var i = 0; i < checkboxs.length; i++) {
+    if (checkboxs[i].checked) {
+      okay = true;
+      
+    } else {
+      
+    }
+
+  }
+  // Check if none of the boxes were checked
+  if (!okay) {
+    document.getElementById("ansSubmit7").style.backgroundColor="red";
+   	window.alert("Please select an answer");
+  }else if(ans !== okay){
+  document.getElementById("ansSubmit7").style.backgroundColor="red";
+  window.alert("Try Again")
+  }else { document.getElementById("ansSubmit7").style.backgroundColor="green";
+  }
+}
+
+window.ansSubmit8 = function() {
+	var ans = document.getElementById("a32").checked;
+  var checkboxs = document.getElementsByClassName("question8");
+  var okay = false;
+  for (var i = 0; i < checkboxs.length; i++) {
+    if (checkboxs[i].checked) {
+      okay = true;
+      
+    } else {
+      
+    }
+
+  }
+  // Check if none of the boxes were checked
+  if (!okay) {
+    document.getElementById("ansSubmit8").style.backgroundColor="red";
+   	window.alert("Please select an answer");
+  }else if(ans !== okay){
+  document.getElementById("ansSubmit8").style.backgroundColor="red";
+  window.alert("Try Again")
+  }else { document.getElementById("ansSubmit8").style.backgroundColor="green";
+  }
+}
+
+window.ansSubmit9 = function() {
+	var ans = document.getElementById("a35").checked;
+  var checkboxs = document.getElementsByClassName("question9");
+  var okay = false;
+  for (var i = 0; i < checkboxs.length; i++) {
+    if (checkboxs[i].checked) {
+      okay = true;
+      
+    } else {
+      
+    }
+
+  }
+  // Check if none of the boxes were checked
+  if (!okay) {
+    document.getElementById("ansSubmit9").style.backgroundColor="red";
+   	window.alert("Please select an answer");
+  }else if(ans !== okay){
+  document.getElementById("ansSubmit9").style.backgroundColor="red";
+  window.alert("Try Again")
+  }else { document.getElementById("ansSubmit9").style.backgroundColor="green";
+  }
+}
+
+window.ansSubmit10 = function() {
+	var ans = document.getElementById("a37").checked;
+  var checkboxs = document.getElementsByClassName("question10");
+  var okay = false;
+  for (var i = 0; i < checkboxs.length; i++) {
+    if (checkboxs[i].checked) {
+      okay = true;
+      
+    } else {
+      
+    }
+
+  }
+  // Check if none of the boxes were checked
+  if (!okay) {
+    document.getElementById("ansSubmit10").style.backgroundColor="red";
+   	window.alert("Please select an answer");
+  }else if(ans !== okay){
+  document.getElementById("ansSubmit10").style.backgroundColor="red";
+  window.alert("Try Again")
+  }else { document.getElementById("ansSubmit10").style.backgroundColor="green";
+  }
+}
+
+function myTip1() {
+   window.alert("Declaration as in a period in a sentence... Sound familiar? "); 
+}
+
+function myTip2() {
+   window.alert("If you are having problems with this you should take a look at the course cheatsheet provide you might find a hint there"); 
+}
+
+function myTip3() {
+   window.alert("Remember, values of this property can be added in a clockwise method"); 
+}
+
+function myTip4() {
+   window.alert("Try pulling up the source code and peak at the stylesheet. If that hint doesnt work you can look over on www.Stackoverflow.com"); 
+}
+
+function myTip5() {
+   window.alert("This is another case of finding a correct property - value pairs, if you are having trouble with this take a look at some examples on the course cheatsheet."); 
+}
+function myTip6() {
+   window.alert("Try pulling up the source code, You can do this by pressing CRTL + U "); 
+}
+
+function myTip7() {
+   window.alert("This is another case of finding a correct property - value pairs, if you are having trouble with this take a look at some examples on the course cheatsheet."); 
+}
+
+function myTip8() {
+   window.alert("Take a miute and look at the code in question... Anything fitting together? If not check out the course cheatsheet"); 
+}
+
+function myTip9() {
+   window.alert("Dont panic! A key skill of a Web Developer is the Attention to Detail."); 
+}
+
+function myTip10() {
+   window.alert("This might look tricky, Try reading the question again to pull out the elemental key. You are free to use any resources, or checkback to the Intro to Web Development (B) CSS video"); 
+}
+
